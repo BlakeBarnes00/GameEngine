4d5353dcb2601d1d0edcbbb074148bdca3c49286
Ellis Barnes <ellisbarnes00@gmail.com>
2020-09-24 18:15:33 -0400

    Limited frames per second to 60

--- main.cpp
+++ main.cpp
@@ -2,13 +2,28 @@
 #include "Game.h"
 
 int main(int argc, char* argv[]) {
-	Game* game = nullptr;
-	game = new Game("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+	Game* game = new Game("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);;
 
 	while (game->running()) {
+		frameStart = SDL_GetTicks();
+
+
 		game->eventHandler();
 		game->update();
 		game->render();
+		
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
